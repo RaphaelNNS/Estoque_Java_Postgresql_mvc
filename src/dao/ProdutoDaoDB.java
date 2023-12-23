7d83b8dc88403c57c21ffd93c40021d09f92aef0
Raphael Nunes da Silva <77383914+RaphaelNNS@users.noreply.github.com>
2023-12-22 23:59:26 -0300

    .

--- src/dao/ProdutoDaoDB.java
+++ src/dao/ProdutoDaoDB.java
@@ -1,15 +1,43 @@
 package dao;
 
+import DB.ConnectionFactory;
 import domain.Cliente;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ProdutoDaoDB implements IProdutoDao{
 
     @Override
     public Integer cadastrar(Cliente cliente) throws Exception {
-        
-        return null;
+        Connection connection = null;
+        PreparedStatement stm = null;
+        try {
+            connection = ConnectionFactory.getConnection();
+            String query = getSqlCadastrar();
+            stm = connection.prepareStatement(query);
+            adicionarParametrosCadastrar(stm);
+            stm.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            closeConnection(connection, stm, result);
+        }
+        return;
+    }
+
+    private void adicionarParametrosCadastrar(PreparedStatement stm) {
+
+    }
+
+    private String getSqlCadastrar() {
+        StringBuilder query = new StringBuilder();
+        query.append("INSERT INTO tb_produtos (codigo, nome, descricao, valor, quantidade) ");
+        query.append("VALUES (?, ?, ?, ?, ?)");
+        return query.toString();
     }
 
     @Override
@@ -35,5 +63,23 @@ public class ProdutoDaoDB implements IProdutoDao{
     @Override
     public Integer quantidade() throws Exception {
         return null;
+    }
+
+
+    private void closeConnection(Connection connection, PreparedStatement stm, ResultSet result) {
+        try{
+            if( result != null && !result.isClosed()){
+                result.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+            if (stm != null && !stm.isClosed()) {
+                stm.close();
+            }
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+
+        }
     }
 }
